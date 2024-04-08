f9e85f8e253bc797d2a07009c7316aa60f117c11
HoangVM2k2 <hoangvmhe161685@fpt.edu.vn>
2024-04-08 22:34:40 +0700

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # NLP_Project
+
+
+# How to run?
+
+```bash
+python3 -m venv NLP
+```
+```bash
+NLP\Scripts\activate.bat
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="HoangVM",
+    author_email="vuminhhoang2002.lequydon@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
